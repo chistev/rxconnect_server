c770d6686c0db098b9af771d22e70198b5b1caef
chistev <stephenowabie@gmail.com>
2024-11-18 19:50:53 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 5000;
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST'],
+}));
+
+app.use(express.json());
+
+if (!process.env.MONGO_URI) {
+    console.error('MONGO_URI is not defined');
+    process.exit(1); 
+}
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server running at http://localhost:${PORT}`);
+});
+ 
